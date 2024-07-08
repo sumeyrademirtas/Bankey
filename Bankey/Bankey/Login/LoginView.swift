627051cfe0e57a2e2e9ad2f914dc78e9c13692df
Sumeyra Demirtas <smyrdmrts@gmail.com>
2024-07-08 12:07:25 +0300

    feat: add password visibility toggle
    # created UITextField+SecureToggle to add password toggle functionality.
    # defined `enablePasswordToggle` method to set up the toggle button with eye icons.
    # added `togglePasswordView` method to toggle the secure text entry and button state.
    # integrated password toggle in LoginView by calling `passwordTextField.enablePasswordToggle()`.

--- Bankey/Bankey/Login/LoginView.swift
+++ Bankey/Bankey/Login/LoginView.swift
@@ -51,6 +51,7 @@ extension LoginView {
         passwordTextField.placeholder = "Password"
         passwordTextField.isSecureTextEntry = true
         passwordTextField.delegate = self
+        passwordTextField.enablePasswordToggle()
         
         dividerView.translatesAutoresizingMaskIntoConstraints = false
         dividerView.backgroundColor = .secondarySystemFill
