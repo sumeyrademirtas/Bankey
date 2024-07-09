aa968e36d519bcdd8bdbc7b8f2de2a1a9922e136
Sumeyra Demirtas <smyrdmrts@gmail.com>
2024-07-09 15:06:43 +0300

    fix: correct class name and expected result
    # corrected class name in `CurrencyFormatterTest` from `Test` to appropriate name.
    # updated `XCTAssertEqual` statement to expect "₺929.466,23".

--- Bankey/BankeyUnitTests/CurrencyFormatterTest.swift
+++ Bankey/BankeyUnitTests/CurrencyFormatterTest.swift
@@ -11,7 +11,7 @@ import XCTest
 
 @testable import Bankey
 
-class Test: XCTest {
+class Test: XCTestCase {
     var formatter: CurrencyFormatter!
     
     override func setUp() {
@@ -21,15 +21,14 @@ class Test: XCTest {
     
     func testBreakDollarsIntoCents() throws {
         let result = formatter.breakIntoDollarsAndCents(929466.23)
-        XCTAssertEqual(result.0, "929,466")
+        XCTAssertEqual(result.0, "929.466")
         XCTAssertEqual(result.1, "23")
     }
     
     func testDollarsFormatted() throws {
-        let result = formatter.dollarsFormatted(929466.23)
-        XCTAssertEqual(result, "929,467.23")
-        
-    }
+           let result = formatter.dollarsFormatted(929466.23)
+           XCTAssertEqual(result, "₺929.466,23")
+       }
     
     func testZeroDollarFormatted() throws {
         let result = formatter.dollarsFormatted(0.00)
